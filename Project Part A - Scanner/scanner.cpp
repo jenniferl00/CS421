1b6c191aa8e98c6ffc948df5d5218d56dce4ce3d
jenniferl00 <73903034+jenniferl00@users.noreply.github.com>
2021-04-05 16:46:46 -0700

    Update scanner.cpp

--- Project Part A - Scanner/scanner.cpp
+++ Project Part A - Scanner/scanner.cpp
@@ -1,9 +1,24 @@
 bool word (string s)
 {
+  // declare instance variables
+  int state = 0; // state = current state of the dfa, begins at 0 (q0)
+  int charpos = 0; // character position of string s
+  
+  // while loop will run until it reaches the end of the string s
+  while (s[charpos] != '\0')
+  {
+    // checks if the first character is a vowel
+    if (state == 0 && (s[charpos] == 'a' || s[charpos] == 'i' || s[charpos] == 'u' || s[charpos] == 'e' || s[charpos] == 'o' || s[charpos] == 'I' || s[charpos] == 'E'))
+      state = 1;
+    else if
+      state = 2;
+  }
+  
+  /**********************************************
   int state = 0;
   int charpos = 0;
   
-  /* replace the following todo the word dfa */
+  // replace the following todo the word dfa
   while (s[charpos] != '\0')
   {
     if (state == 0 && s[charpos] == 'a')
@@ -22,4 +37,5 @@ bool word (string s)
     return true; // end in a file state
   else
     retun false;
+  **********************************************/
 }
