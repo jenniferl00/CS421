7cd08cc5f55824a0a645123fec09b9db320a7f28
jenniferl00 <73903034+jenniferl00@users.noreply.github.com>
2021-04-05 17:17:00 -0700

    Update scanner.cpp

--- Project Part A - Scanner/scanner.cpp
+++ Project Part A - Scanner/scanner.cpp
@@ -7,12 +7,55 @@ bool word (string s)
   // while loop will run until it reaches the end of the string s
   while (s[charpos] != '\0')
   {
-    // checks if the first character is a vowel
+    // transitions from the initial state (q0)
     if (state == 0 && (s[charpos] == 'a' || s[charpos] == 'i' || s[charpos] == 'u' || s[charpos] == 'e' || s[charpos] == 'o' || s[charpos] == 'I' || s[charpos] == 'E'))
-      state = 1;
-    else if
+      state = 1; 
+    else if (state == 0 && (s[charpos] == 'b' || s[charpos] == 'g' || s[charpos] == 'h' || s[charpos] == 'k' || s[charpos] == 'm' || s[charpos] == 'p' || s[charpos] == 'r'))
+      state = 4;
+    else if (state == 0 && (s[charpos] == 'd' || s[charpos] == 'j' || s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
+      state = 5;
+    else if (state == 0 && s[charpos] == 'c')
+      state = 3;
+    else if (state == 0 && s[charpos] == 's')
+      state = 6;
+    else if (state == 0 && s[charpos] == 't')
+      state = 7;
+    
+    /* transitions from state 1 (q0q1)
+    state 2 = q0qy
+    state 3 = qc
+    state 4 = qy
+    state 5 = qsa
+    state 6 = qs
+    state 7 = qt
+    */ 
+    else if (state == 1 && (s[charpos] == 'b' || s[charpos] == 'g' || s[charpos] == 'h' || s[charpos] == 'k' || s[charpos] == 'm' || s[charpos] == 'p' || s[charpos] == 'r'))
+      state = 4;
+    else if (state == 1 && (s[charpos] == 'd' || s[charpos] == 'j' || s[charpos] == 'w' || s[charpos] == 'y' || s[charpos] == 'z'))
+      state = 5;
+    else if (state == 1 && s[charpos] == 'c')
+      state = 3;
+    else if (state == 1 && s[charpos] == 's')
+      state = 6;
+    else if (state == 1 && s[charpos] == 't')
+      state = 7;
+    
+    else if (state == 1 && s[charpos] == 'n')
       state = 2;
-  }
+    
+    // transitions from state 2 (q0qy)
+    
+    else
+        return false; // occurs when none of the characters meets the if/else-if statement requirements
+    
+    charpos++; // changes character position in the string s
+  } // end of while
+  
+  // where did i end up?
+  if (state == 0 || state == 1 || state == 2)
+    return true;
+  else 
+    return false;
   
   /**********************************************
   int state = 0;
